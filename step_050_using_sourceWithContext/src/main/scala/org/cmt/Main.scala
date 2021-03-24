723f909edc745516b6a2b79b2e517097b2547194
eloots <eric.loots@gmail.com>
2021-03-24 15:40:51 +0100

    Refactor as source with context example
    - The example was confusing as the sample data was a tuple containing
      a UUID, which cuold easily be mistaken to represent context information.
    - Changed that to use the UUID as context info and another tuple of
      and Int and a Double to represent data.

--- step_050_using_sourceWithContext/src/main/scala/org/cmt/Main.scala
+++ step_050_using_sourceWithContext/src/main/scala/org/cmt/Main.scala
@@ -5,7 +5,6 @@ import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Sink, Source, SourceWithContext}
 import org.cmt.StreamLogExtensions._
 import org.slf4j.{ LoggerFactory}
-import akka.event.LoggingAdapter
 
 import java.util.UUID
 import scala.util.Random
@@ -14,7 +13,41 @@ object Main {
 
   def extractContext[Data, Ctx](element: (Data, Ctx)): Ctx = element._2
   def extractData[Data, Ctx](element: (Data, Ctx)): Data = element._1
-  
+
+  /**
+   * A source data based on tuples of two values:
+   *   - the first one, an Int between 0 and 999,
+   *   - the second one a Double between 0.0d and 100000.0d
+   */
+  val dataSource: Source[(Int, Double), NotUsed] = {
+    val scale = 100000
+    val sourceOfInts = Source.fromIterator(() => Iterator.continually(Random.nextInt(1000)))
+    val sourceOfDouble = Source.fromIterator(() => Iterator.continually(Random.nextInt(scale + 1)/scale.toDouble))
+    sourceOfInts.zip(sourceOfDouble)
+  }
+
+  /**
+   * A source of some context data modelled as a series of randomly generated UUIDs
+   */
+  val contextSource: Source[UUID, NotUsed] =
+    Source.fromIterator(() => Iterator.continually(UUID.randomUUID()))
+
+  /**
+   * A source of data & context
+   */
+  val sourceDataWithContext: Source[((Int, Double), UUID), NotUsed] = dataSource.zip(contextSource)
+
+
+  /**
+    * We demonstrate `asSourceWithContext` which expects elements to consist of tuples
+    * where the first element contains data and the second element contains some
+    * context (metadata)
+    * 
+    * Using `asSourceWithContext` allows us to 'ship' the context transparently (read
+    * invisible to the core data processing part of the stream processing) and
+    * to put it 'back' after the data processing is complete.
+    *
+    */
   def main(args:Array[String]): Unit = {
     implicit val system: ActorSystem = ActorSystem("akka-stream-system")
     implicit val loggingAdapter = system.log
@@ -22,17 +55,22 @@ object Main {
 
     implicit val logger = LoggerFactory.getLogger(getClass)
 
-    val sourceIds = Source.fromIterator(() => Iterator.continually(UUID.randomUUID()))
-    val data = Source.fromIterator(() => Iterator.continually(Random.nextInt(1000)))
-    val inputValues = sourceIds.zip(data)
-    val st = inputValues
-        .zipWithIndex
+    /**
+      * Some extremely sofisticated business logic
+      *
+      * @param i: A business Integer value
+      * @param d: Some business Double value
+      * @return (i, d + i)
+      */
+    def myBusinessLogic(i: Int, d: Double): (Int, Double) = (i, d + i)
+
+    val st = sourceDataWithContext
         .take(10)
         .logInfo("Elements Sourced")
         .asSourceWithContext(extractContext).map(extractData)
-        .map{ case (key, n) => (key, n * 2)} // "Business Logic"
+        .map{ case (i, d) => myBusinessLogic(i, d)} // Execute "Business Logic"
         .logSWCInfo("Elements Inside")
-        .asSource
+        .asSource                                   // Restore context info
         .logInfo("Elements Processed")
         .runWith(Sink.ignore)
 
